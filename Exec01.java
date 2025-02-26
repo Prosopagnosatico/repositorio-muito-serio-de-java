210c95a787ceca1d00b0b57f7877e59a7f0391cd
Prosopagnosatico <168598761+Prosopagnosatico@users.noreply.github.com>
2025-02-26 12:53:42 -0300

    Add files via upload

---
+++ Exec01.java
@@ -0,0 +1,12 @@
+/*
+public class Exec01 {
+    String nome = "Meuquesedeque";
+    int idade = 16;
+    float altura = 169;
+    int cpf = 56845698708;
+
+    // System.out.println("Nome: " + nome);
+    // System.out.println("Idade: " + idade);
+    // System.out.println("Altura: " + altura);
+    // System.out.println("Cpf: " + cpf)
+}*/
---
+++ Exec02.java
@@ -0,0 +1,19 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Exec02 {
+
+    public static void main (String[] args){
+        char sexo = 'm';
+        byte idade = 17;
+        short codigo = 258;
+        float nota = 0.2f;
+        int alunos = 100, classes = 10;
+        long habitantes = 23;
+        double dolar = 5.00;
+        boolean alternativa = false;
+
+        System.out.println("Sexo: " + sexo + " / " +  "Idade: " + idade + " / " + "Código: " + codigo);
+        System.out.println("Nota :" + nota + " / " + "Alunos: " + alunos + " / " + "Classes: " + classes);
+        System.out.println("Habitantes: " + habitantes + " / " + "Dolar: " + dolar + " / " + "Alternativa: " + alternativa);
+}
+}
